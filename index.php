f91e37779f2df050ea5ab9311b35fe48554f72a1
Ruben Verdoes <horsedrowner@gmail.com>
2013-01-23 14:58:33 +0100

    Bootstrap, router, basic index

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+spl_autoload_register(function ($class) {
+    include 'php/' . $class . '.php';
+});
+?>
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Imdex</title>
+<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+<div class="container">
+	<div class="row">
+		<div class="span2">
+			<ul class="nav nav-list">
+				<li class="nav-header">CURRENT FOLDER
+				<li><a href="..">Go up</a>
+				<li><a href="img">img</a>
+			</ul>
+		</div>
+		<div class="span10">
+			<h1>Hi</h1>
+			<dl>
+				<dt>Current directory
+				<dd><p><?php echo Path::GetFullPath($_SERVER["SCRIPT_NAME"]);?> 
+			</dl>
+			<pre>
+<?php print_r($_SERVER);?>
+			</pre>
+		</div>
+	</div>
+</div>
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="/js/bootstrap.min.js"></script>
