0f7a27d0d5c955ef5b0633fa39da70bd887c982d
Ruben Verdoes <horsedrowner@gmail.com>
2016-08-28 16:32:18 +0200

    Style changes

--- app/helpers.rb
+++ app/helpers.rb
@@ -4,6 +4,11 @@ module Imdex
       URI.encode(value).gsub('+', '%2B')
     end
 
+    def css_url(value)
+      return nil if value.nil?
+      CGI.escape(value).gsub('+', '%20')
+    end
+
     ##
     # Determines the last modified date of the file corresponding to the 
     # specified template.
